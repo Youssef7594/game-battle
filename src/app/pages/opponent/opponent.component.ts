47ecc811fa2330f49587d5f7f799f45e430b3f73
Youssef7594 <Yousseffaras75020@gmail.com>
2025-07-19 17:16:45 +0200

    Add opponent attribut

--- src/app/pages/opponent/opponent.component.ts
+++ src/app/pages/opponent/opponent.component.ts
@@ -7,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrl: './opponent.component.css'
 })
 export class OpponentComponent {
+  opponent: opponents[] = [] ;
+
+}
+
+
+export class opponents{
+  id : number ;
+  nom : string ;
+  attack : number ;
+  pv : number ;
+
+
+
+
+  constructor(id: number, nom: string, pv: number ){
+    this.id = id;
+    this.nom = nom;
+    this.pv = pv;
+    // Génère une attaque aléatoire entre 30 et 60
+    this.attack = Math.floor(Math.random() * (60 - 30 + 1)) + 30;
+  }
+
+
+
+  
+
 
 }
