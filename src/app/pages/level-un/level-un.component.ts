b99eb252ac0128115392d5d5614d895adb851263
Youssef7594 <Yousseffaras75020@gmail.com>
2025-07-27 16:24:39 +0200

    link and configuration

--- src/app/pages/level-un/level-un.component.ts
+++ src/app/pages/level-un/level-un.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { PlayerComponent } from './pages/player/player.component';
+import { PlayerComponent } from '../player/player.component';
+
 
 @Component({
   selector: 'app-level-un',
+  standalone: true,
   imports: [],
   templateUrl: './level-un.component.html',
   styleUrl: './level-un.component.css'
