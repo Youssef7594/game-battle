ff71a153b0937d65a27bce4b2a49937581bd952b
Youssef7594 <Yousseffaras75020@gmail.com>
2025-08-23 12:42:23 +0200

    New component lose and level-two

---
+++ src/app/pages/level-two/level-two.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-level-two',
+  imports: [],
+  templateUrl: './level-two.component.html',
+  styleUrl: './level-two.component.css'
+})
+export class LevelTwoComponent {
+
+}
---
+++ src/app/pages/lose/lose.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lose',
+  imports: [],
+  templateUrl: './lose.component.html',
+  styleUrl: './lose.component.css'
+})
+export class LoseComponent {
+
+}
