a210e154eef341e6813bc6e08c23853407dc418d
Youssef7594 <Yousseffaras75020@gmail.com>
2025-09-03 12:24:07 +0200

    Page level Two

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LevelUnComponent } from './pages/level-un/level-un.component';
+import { LevelTwoComponent } from './pages/level-two/level-two.component';
 
 export const routes: Routes = [{
     path : '',
@@ -12,6 +13,9 @@ export const routes: Routes = [{
 },{
     path : 'level-one',
     component : LevelUnComponent,
+},{
+    path : 'level-two',
+    component : LevelTwoComponent,
 },{
     path : '**',
     component : HomeComponent, 
