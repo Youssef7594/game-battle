3227957e56426c4795407a39a774e6b4b8c82de6
Youssef7594 <Yousseffaras75020@gmail.com>
2025-09-06 14:17:53 +0200

    Opponent of level two

--- src/app/pages/player/player.component.ts
+++ src/app/pages/player/player.component.ts
@@ -13,11 +13,13 @@ import { Router } from '@angular/router';
 export class PlayerComponent {
   player: Players;
   adversaire: Players;
+  adversairetwo: Players;
 
   constructor(private router:Router) {
     // ✅ Création d’un joueur
     this.player = new Players(1, "Hero", 100);
-    this.adversaire = new Players(2, "Opponent", 100)
+    this.adversaire = new Players(2, "Opponent", 100);
+    this.adversairetwo = new Players(3, "Opponent-two", 150)
   }
 
   attaquer() {
@@ -25,6 +27,20 @@ export class PlayerComponent {
 
     // Vérifier si l'adversaire est mort
     if (this.adversaire.pv <= 0) {
+      this.router.navigate(['/lose']);
+    }
+
+    // Vérifier si le joueur est mort
+    if (this.player.pv <= 0) {
+      this.router.navigate(['/level-two']);
+    }
+  };
+
+  attaquerTwo() {
+    this.adversairetwo.pv -= this.player.attack;
+
+    // Vérifier si l'adversaire est mort
+    if (this.adversairetwo.pv <= 0) {
       this.router.navigate(['/lose']);
     }
 
